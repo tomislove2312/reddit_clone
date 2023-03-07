6c9008ab18a5d71858a9f90381986caa2295ad4d
Tomislav Marakovic <tomislav.marakovic@aol.com>
2023-03-07 15:32:44 +0100

    First commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,11 @@
+import { ChakraBaseProvider } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
+import { theme } from "../chakra/theme";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraBaseProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraBaseProvider>
+  );
+}
